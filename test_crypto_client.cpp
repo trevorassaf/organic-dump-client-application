d6e201fe15fc3e4fff2d32e8db66ee26d06aa930
Trevor Assaf <bozkurtus@google.com>
2019-09-30 09:32:26 -0700

    Replace with Tls abstractions

--- test_crypto_client.cpp
+++ test_crypto_client.cpp
@@ -1,10 +1,3 @@
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <errno.h>
-
 #include <cassert>
 #include <cstdlib>
 #include <memory>
@@ -13,9 +6,10 @@
 #include <gflags/gflags.h>
 #include <glog/logging.h>
 
-#include  <openssl/bio.h>
-#include  <openssl/ssl.h>
-#include  <openssl/err.h>
+#include <TlsClientFactory.h>
+#include <TlsClient.h>
+#include <TlsConnection.h>
+#include <TlsUtilities.h>
 
 DEFINE_string(cert, "", "Certificate PEM");
 DEFINE_string(key, "", "Key PEM");
@@ -24,126 +18,15 @@ DEFINE_string(ipv4, "", "IPv4 address of server");
 DEFINE_int32(port, -1, "Port");
 DEFINE_string(message, "", "Message");
 
-namespace
-{
-bool InitCtx(SSL_CTX **outCtx)
-{
-    const SSL_METHOD *method;
-
-    OpenSSL_add_all_algorithms();
-    SSL_load_error_strings();
-
-    method = TLSv1_2_client_method();
-    *outCtx = SSL_CTX_new(method);
-    if (*outCtx == nullptr)
-    {
-        ERR_print_errors_fp(stderr);
-        return false;
-    }
-
-    return true;
-}
-
-bool LoadClientCerts(SSL_CTX *ctx, const char *certPath, const char *keyPath, const char *caPath)
-{
-    if (SSL_CTX_load_verify_locations(ctx, caPath, nullptr) != 1)
-    {
-        LOG(ERROR) << "Failed to load client CA file: " << caPath;
-        return false;
-    }
-
-    if (SSL_CTX_use_certificate_file(ctx, certPath, SSL_FILETYPE_PEM) != 1)
-    {
-        LOG(ERROR) << "Failed to load client cert " << certPath;
-        return false;
-    }
-
-    if (SSL_CTX_use_PrivateKey_file(ctx, keyPath, SSL_FILETYPE_PEM) != 1)
-    {
-        LOG(ERROR) << "Failed to load private key: " << keyPath;
-        return false;
-    }
-
-
-    if (SSL_CTX_check_private_key(ctx) != 1)
-    {
-        LOG(ERROR) << "Private key does not agree with cert";
-        return false;
-    }
-
-/*
-    SSL_CTX_set_mode(ctx, SSL_MODE_AUTO_RETRY);
-*/
-    SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, nullptr);
-    SSL_CTX_set_verify_depth(ctx, 1);
-
-    return true;
-}
-
-bool ConnectServer(const char *serverIpv4, uint16_t port, uint32_t *outServer)
-{
-    assert(outServer);
-
-    struct sockaddr_in addr;
-    memset(&addr, 0, sizeof(addr));
-    inet_pton(AF_INET, serverIpv4, &(addr.sin_addr));
-
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
-
-    int sd = socket(PF_INET, SOCK_STREAM, 0);
-    if (sd < 0)
-    {
-        LOG(ERROR) << "Failed to initialize socket";
-        return false;
-    }
-
-    if (connect(
-          sd,
-          reinterpret_cast<struct sockaddr*>(&addr),
-          sizeof(addr)) < 0)
-    {
-        LOG(ERROR) << "Failed to connect server";
-        close(sd);
-        return false;
-    }
-
-    *outServer = sd;
-    return true;
-}
-
-bool ShowCerts(SSL *ssl)
-{
-    X509 *cert;
-    std::unique_ptr<char[]> x509Line;
-
-    cert = SSL_get_peer_certificate(ssl);
-    if (cert == nullptr)
-    {
-        LOG(ERROR) << "Failed to get peer certificate";
-        return false;
-    }
-
-    LOG(ERROR) << "Server TLS certificate...";
-    x509Line.reset(X509_NAME_oneline(X509_get_subject_name(cert), 0, 0));
-    LOG(ERROR) << "- Subject name: " << x509Line.get();
-    x509Line.reset(X509_NAME_oneline(X509_get_issuer_name(cert), 0, 0));
-    LOG(ERROR) << "- Issuer: " << x509Line.get();
-    X509_free(cert);
-
-    return true;
-}
-
-} // anonymous namespace
+using network::TlsClientFactory;
+using network::TlsClient;
+using network::TlsConnection;
 
 int main(int argc, char **argv)
 {
     google::ParseCommandLineFlags(&argc, &argv, false);
     FLAGS_logtostderr = 1;
     google::InitGoogleLogging(argv[0]);
-
-    /* Initializing OpenSSL */
-    ERR_load_BIO_strings();
 
     LOG(INFO) << "Cert: " << FLAGS_cert;
     LOG(INFO) << "Key: " << FLAGS_key;
@@ -152,81 +35,54 @@ int main(int argc, char **argv)
     LOG(INFO) << "Port: " << FLAGS_port;
     LOG(INFO) << "Message: " << FLAGS_message;
 
-    SSL_CTX *ctx = nullptr;
-    SSL *ssl = nullptr;
-    uint32_t serverFd = -1;
-    char message[100];
-    int sslReadResult = -1;
+    /* Initializing OpenSSL */
+    ERR_load_BIO_strings();
+    OpenSSL_add_all_algorithms();
+    SSL_load_error_strings();
 
-    LOG(INFO) << "Before InitCtx";
+    TlsClient client;
+    TlsClientFactory factory;
 
-    if (!InitCtx(&ctx))
+    if (!factory.Create(
+        FLAGS_ipv4,
+        FLAGS_port,
+        FLAGS_cert,
+        FLAGS_key,
+        FLAGS_ca,
+        &client))
     {
-        LOG(ERROR) << "Failed to initilize TLSv1.2 context";
-        goto error;
+        LOG(ERROR) << "Failed to create TlsClient";
+        return EXIT_FAILURE;
     }
 
-    LOG(INFO) << "Before LoadClientCerts";
-
-    if (!LoadClientCerts(ctx, FLAGS_cert.c_str(), FLAGS_key.c_str(), FLAGS_ca.c_str()))
+    TlsConnection cxn;
+    if (!client.Connect(&cxn))
     {
-        LOG(ERROR) << "Failed to load client certs";
-        goto error;
+        LOG(ERROR) << "TlsClient failed to connect to server";
+        return EXIT_FAILURE;
     }
 
-    LOG(INFO) << "Before ConnectServer";
-
-    if (!ConnectServer(FLAGS_message.c_str(), FLAGS_port, &serverFd))
+    if (!SendTlsMessage(&cxn, FLAGS_message))
     {
-        LOG(ERROR) << "Failed to connect server";
-        goto error;
+        LOG(ERROR) << "Failed to send message to TLS server: " << FLAGS_message;
+        return EXIT_FAILURE;
     }
 
-    LOG(INFO) << "Client succeeded in connect() to server";
+    LOG(INFO) << "Sent message to TLS server!";
 
-    ssl = SSL_new(ctx);
-    SSL_set_fd(ssl, serverFd);
-    if (SSL_connect(ssl) != 1)
+    uint8_t read_buffer[256];
+    std::string server_message;
+    if (!ReadTlsMessage(
+          &cxn,
+          read_buffer,
+          sizeof(read_buffer),
+          &server_message))
     {
-        LOG(ERROR) << "Failed to perform SSL handshake";
-        ERR_print_errors_fp(stderr);
-        goto error;
+        LOG(ERROR) << "Failed to read TLS message from server: " << server_message;
+        return EXIT_FAILURE;
     }
 
-    LOG(INFO) << "Client succeeded in SSL_connect()";
+    LOG(INFO) << "Received message from TLS server: " << server_message;
 
-    /*
-    if (!ShowCerts(ssl))
-    {
-        LOG(ERROR) << "Failed to print peer cert";
-        goto error;
-    }
-
-    LOG(INFO) << "Client succeeded in ShowCerts()";
-    */
-
-    if (SSL_write(ssl, FLAGS_message.c_str(), strlen(FLAGS_message.c_str())) < 1)
-    {
-        LOG(ERROR) << "Failed to send message";
-        goto error;
-    }
-
-    LOG(INFO) << "Client succeeded in write()";
-
-    memset(message, 0, sizeof(message));
-    sslReadResult = SSL_read(ssl, message, sizeof(message));
-    if (sslReadResult < 1)
-    {
-        LOG(ERROR) << "Failed to read message";
-        goto error;
-    }
-
-    LOG(INFO) << "Server message: " << message;
     return EXIT_SUCCESS;
-
-error:
-    SSL_free(ssl);
-    close(serverFd);
-    SSL_CTX_free(ctx);
-    return EXIT_FAILURE;
 }
